afca6c47242d547b77c70779d2e2a5caa8f8acbe
Darvesh Gorhe <darveshgorhe@Darveshs-MacBook-Pro.local>
2023-05-30 10:53:26 -0700

    files: script for creating rulegraph (i.e. directed acyclic graph showing relationships between all the rules) and the output of the script

---
+++ pipeline/rulegraph.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+snakemake --rulegraph > rulegraph.tmp
+tail -n +10 rulegraph.tmp > rulegraph.txt
+dot -Tpdf rulegraph.txt > rulegraph.pdf
+rm rulegraph.tmp rulegraph.txt
+echo "File saved to rulegraph.pdf"
