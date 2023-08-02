41bb3a2e1572dae1a0a32b829c59b409c56b9c40
Darvesh Sanjeev Gorhe <dsg2157@bader.cm.cluster>
2023-08-02 18:07:50 -0400

    file: runs Snakmake interactively in current SSH shell rather than dispatching sbatch job

---
+++ pipeline/run_pipeline_interactive.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+snakemake \
+--snakefile Snakefile \
+--use-conda \
+--rerun-incomplete \
+-j 128 \
+--cluster-config cluster.yaml \
+--configfile config.yaml \
+--cluster "sbatch -A {cluster.account} -c {cluster.cpus} -t {cluster.time} -N {cluster.nodes} --mem {cluster.mem} --output {cluster.output} --error {cluster.error}" \
