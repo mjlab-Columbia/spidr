5d8816a2f4dfa441b5fe8bdddda7542249af5aaa
Darvesh Sanjeev Gorhe <dsg2157@bader.cm.cluster>
2023-08-17 13:20:22 -0400

    feat: removed multiqc restart and added rule profiling to measure time elapsed by each rule

--- pipeline/run_pipeline_interactive.sh
+++ pipeline/run_pipeline_interactive.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 snakemake \
---snakefile Snakefile \
---use-conda \
---rerun-incomplete \
--j 128 \
---cluster-config cluster.yaml \
---configfile config.yaml \
---cluster "sbatch -A {cluster.account} -c {cluster.cpus} -t {cluster.time} -N {cluster.nodes} --mem {cluster.mem} --output {cluster.output} --error {cluster.error}" \
--R multiqc \
---restart-times 3 \
+    --snakefile Snakefile \
+    --use-conda \
+    --rerun-incomplete \
+    -j 128 \
+    --cluster-config cluster.yaml \
+    --configfile config.yaml \
+    --cluster "sbatch -A {cluster.account} -c {cluster.cpus} -t {cluster.time} -N {cluster.nodes} --mem {cluster.mem} --output {cluster.output} --error {cluster.error}" \
+    --profile-stats 
