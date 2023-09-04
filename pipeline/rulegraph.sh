485031d335b21d17322fe36b616d4c9e1658e7b2
Darvesh Gorhe <darveshgorhe@gmail.com>
2023-09-04 12:54:12 -0400

    deprecation: removing old files (all were renamed)

--- pipeline/rulegraph.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-snakemake --rulegraph > rulegraph.tmp
-tail -n +10 rulegraph.tmp > rulegraph.txt
-dot -Tpdf rulegraph.txt > rulegraph.pdf
-rm rulegraph.tmp rulegraph.txt
-echo "File saved to rulegraph.pdf"
