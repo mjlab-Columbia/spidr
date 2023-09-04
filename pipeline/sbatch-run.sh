69815932027b27eb301a33143bf62c1d5561f266
Darvesh Gorhe <darveshgorhe@gmail.com>
2023-09-04 14:40:49 -0400

    bugfix: correct config and cluster yaml files in snakemake call

--- pipeline/sbatch-run.sh
+++ pipeline/sbatch-run.sh
@@ -16,8 +16,8 @@ snakemake \
     --use-conda \
     --rerun-incomplete \
     -j 128 \
-    --cluster-config cluster.yaml \
-    --configfile config.yaml \
+    --cluster-config cluster.generate-targets.yaml \
+    --configfile config.generate-targets.yaml \
     --cluster "sbatch -A {cluster.account} -c {cluster.cpus} -t {cluster.time} -N {cluster.nodes} --mem {cluster.mem} --output {cluster.output} --error {cluster.error}" \
 
 # TODO: Write run command for target bam --> postprocessing outputs
