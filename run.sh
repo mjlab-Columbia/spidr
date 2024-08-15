5e23ea02e03fb8925b3862d793734c40c101541c
Darvesh Gorhe <darveshgorhe@gmail.com>
2024-08-15 17:27:36 -0400

    feat: --dry_run flag allows user to perform dry run without modifying run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,11 @@
 # Get the major version to determine whether --cluster-config or --profile method should be used
 SNAKEMAKE_MAJOR=$(snakemake --version | tr "." "\t" | cut -f 1)
 
+DRY_RUN=""
+if [[ "$1" == "--dry_run" ]]; then
+    DRY_RUN="-n"
+fi
+
 # If we're on Snakemake 8 or later, use profiles, otherwise use cluster config
 # Note: 
 # --cluster-config will eventually be deprecated completely so it will (eventually) be completely removed
@@ -29,12 +34,14 @@ if [[ ${SNAKEMAKE_MAJOR} -lt 8 ]]; then
         --cluster "sbatch -A {cluster.account} -c {cluster.cpus} -t {cluster.time} -N {cluster.nodes} --mem {cluster.mem} --output {cluster.output} --error {cluster.error}" \
         --cluster-cancel scancel \
         --omit-from generate_cluster_ecdfs \
+        ${DRY_RUN} \
         2>&1 | tee spidr.log
 elif [[ ${SNAKEMAKE_MAJOR} -ge 8 ]]; then
     snakemake \
         --profile profiles/linux_hpc \
         --configfile config.yaml \
         --omit-from generate_cluster_ecdfs \
+        ${DRY_RUN} \
         2>&1 | tee spidr.log
 fi
 
