da03b042cabee60c0ad79662bad3a1da9b461a89
Darvesh Gorhe <darveshgorhe@gmail.com>
2025-07-22 17:03:47 -0400

    Improved layout of cDNA histogram and sped up data loading/parsing

--- scripts/python/plot_cdna_histogram.py
+++ scripts/python/plot_cdna_histogram.py
@@ -1,31 +1,31 @@
 import click
-import gzip
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 @click.command()
-@click.option("--read1", "-1", type=click.Path(exists=True), help="Path to text file where each line is length of cDNA in pre-aligned fastq per line in read 1")
-@click.option("--read2", "-2", type=click.Path(exists=True), help="Path to text file where each line is length of cDNA in pre-aligned fastq per line in read 1")
-@click.option("--output", "-o", type=click.Path(), help="Path to output file. Extension will determine file pdf regardless of extension")
+@click.option("--read1", "-1", type=click.Path(exists=True),
+              help="Path to text file where each line is length of cDNA in pre-aligned fastq per line in read 1")
+@click.option("--read2", "-2", type=click.Path(exists=True),
+              help="Path to text file where each line is length of cDNA in pre-aligned fastq per line in read 1")
+@click.option("--output", "-o", type=click.Path(),
+              help="Path to output file. Extension will determine file pdf regardless of extension")
 def main(read1, read2, output) -> None:
 
-    if read1.endswith(".gz"):
-        with gzip.open(read1, "rb") as read1_file:
-            read1_lengths = [int(line) for line in read1_file.readlines()]
-    else:
-        with open(read1, "r") as read1_file:
-            read1_lengths = [int(line) for line in read1_file.readlines()]
+    read1_lengths = np.loadtxt(read1, dtype=np.uint8)
+    read2_lengths = np.loadtxt(read2, dtype=np.uint8)
 
-    if read2.endswith(".gz"):
-        with gzip.open(read2, "rb") as read2_file:
-            read2_lengths = [int(line) for line in read2_file.readlines()]
-    else:
-        with open(read2, "r") as read2_file:
-            read2_lengths = [int(line) for line in read2_file.readlines()]
+    fig, ax = plt.subplots(1, 2, figsize=(12, 5), sharex=False)
+    ax[0].hist(read1_lengths, bins=50)
+    ax[1].hist(read2_lengths, bins=50)
 
-    fig, ax = plt.subplots(1, 2, sharex=True)
-    ax[0].hist(read1_lengths)
-    ax[1].hist(read2_lengths)
+    ax[0].set_title("Read 1 cDNA Lengths")
+    ax[0].set_ylabel("Frequency")
+    ax[0].set_xlabel("cDNA Length")
+
+    ax[1].set_title("Read 2 cDNA Lengths")
+    ax[1].set_ylabel("Frequency")
+    ax[1].set_xlabel("cDNA Length")
 
     fig.tight_layout()
     fig.savefig(output, format="pdf", dpi=300)
